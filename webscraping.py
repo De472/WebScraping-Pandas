087ccb4f0d3c3e3fed3742be794d2663b4752869
Devanir <61068102+De472@users.noreply.github.com>
2020-08-03 02:53:21 -0300

    integração com transform_dados.py

--- webscraping.py
+++ webscraping.py
@@ -1,73 +1,84 @@
 
+#Devanir Ramos Junior
+#email: junior472399@gmai.com
+#cel: 12 99258 9411
+
+#pip install beautifulsoup4
 import bs4
+#pip install requests
 import requests
 
-print("Iniciado.\n\n")
-print("Selecione a opção que deseja:\n")
-#é possivel baixar o padrao TISS atual ou verificar o histórico das versões
-while True:
-    print("1 - Baixar Padrão TISS (atualizado);")
-    escolha = input("2 - Verificar o histórico do Padrão TISS.\n")
-    print()
-    if escolha in "12":
-        if escolha == "1":
-            palavra = "versão"
-            break
+if __name__ == '__main__':
+    print("Iniciado.\n")
+    print("Selecione a opção que deseja:\n")
+    # é possivel baixar o padrao TISS atual ou verificar o histórico das versões
+    while True:
+        print("1 - Baixar Padrão TISS (atualizado);")
+        escolha = input("2 - Verificar o histórico do Padrão TISS.\n")
+        print()
+        if escolha in "12":
+            if escolha == "1":
+                palavra = "versão"
+                break
+            else:
+                palavra = "versões"
+                break
         else:
-            palavra = "versões"
-            break
-    else:
-        print("Por favor, digite \"1\" ou \"2\".\n")
+            print("Por favor, digite \"1\" ou \"2\".\n")
 
-print("Aguarde um momento, por favor.\n")
+    print("Aguarde um momento, por favor.\n")
 
-def webscraping(palavra_chave):
+arquivo = ""
+def webscraping(palavra_chave = "versão", escolha = "1"):
+    #uso no programa de tranformar dados
+    global arquivo
+
     # request para acessar o site e beautifulsoup para fazer tratamentos/filtrar
     site1 = "http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
     site1_request = requests.get(site1).text
     soup1 = bs4.BeautifulSoup(site1_request, "html.parser")
 
-    #procura essa classe específica porque os links estão ai
-    for linha in soup1.find_all("div", class_= "alert alert-icolink"):
+    # procura essa classe específica porque os links estão ai
+    for linha in soup1.find_all("div", class_="alert alert-icolink"):
         string = str(linha)
-
-        #verifica se no texto do link tem "versão" ou "versões" e pega o link da próxima página
+        # verifica se no texto do link tem "versão" ou "versões" e pega o link da próxima página
         if palavra_chave in string.lower():
             string_separada = string.split("href=")
             site2 = string_separada[1].split("\"")
             site2_request = requests.get(site1[:22] + site2[1]).text
             soup2 = bs4.BeautifulSoup(site2_request, "html.parser")
-            #se a escolha foi baixar o TISS atual:
+            # se a escolha foi baixar o TISS atual:
             if escolha == "1":
+                #analisa a tabela da página para encontrar o link do arquivo
                 for linha in soup2.find_all("tr"):
                     string = str(linha)
-
                     if "componente organizacional" in string.lower():
-                        print("Baixando...\n")
+                        print("Baixando...\nPode demorar alguns segundos...\n")
                         string_separada = string.split("href=")
                         file = string_separada[1].split("\"")
                         file_request = requests.get(site1[:22] + file[1])
-                        pdf = open("Padrao_TISS_Atualizado.pdf", 'wb')
+                        arquivo = file[1].split("/")
+                        arquivo = arquivo[-1]
+                        pdf = open(arquivo, 'wb')
                         pdf.write(file_request.content)
                         pdf.close()
-                        print("Versão mais nova do Padrão TISS salva como \"Padrão_TISS_Atualizado.pdf\".")
+                        print("Versão mais nova do Padrão TISS salva como \"" + arquivo + "\"")
                         break
-            #se a escolha foi ver o histórico do TISS
+            # se a escolha foi ver o histórico do TISS
             else:
                 txt = open("Histórico_TISS.txt", "w", encoding="utf-8")
-                
                 for linha in soup2.find_all("tr"):
                     string = str(linha.text)
                     string_separada = string.split("\n")
                     texto = ["Competência", "Publicação", "Início de Vigência", "Limite de Implantação",
-                              "Organizacional", "Conteúdo e Estrutura", "Representação de Conceitos",
+                             "Organizacional", "Conteúdo e Estrutura", "Representação de Conceitos",
                              "Segurança e Privacidade", "Comunicação"]
                     try:
-                        for index in range (len(string_separada)):
+                        for index in range(len(string_separada)):
                             text = (texto[index] + ": " + string_separada[index + 1])
                             print(text)
                             txt.write(text + "\n")
-                    #try/except usado porque a primeira "linha" é uma única string com o cabeçalho da tabela
+                    # try/except usado porque a primeira "linha" é uma única string com o cabeçalho da tabela
                     except:
                         pass
 
@@ -80,6 +91,9 @@ def webscraping(palavra_chave):
 
             break
 
-webscraping(palavra)
 
-print("\nPrograma finalizado.")
+
+
+if __name__ == '__main__':
+    webscraping(palavra, escolha)
+    print("\nPrograma finalizado.")
